dfcd0e3fc4702f8e3d053f8724730f270c5873cf
IlyaAfanasev <gecktor.86@mail.ru>
2023-02-15 23:09:22 +0400

    Task 7

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
 
     }
 
@@ -88,6 +89,21 @@ public class Main {
         System.out.println(totalWeightBreakfastGr+ "гр");
         double toеatlWeightBreakfastKg = totalWeightBreakfastGr / 1000;
         System.out.println(toеatlWeightBreakfastKg + "кг");
+
+
+    }
+    public static void task7() {
+        byte totalWeightKg = 7;
+        short totalWeightGr = (short) (totalWeightKg*1000);
+        short weightGr1= 250;
+        short weightGr2 = 500;
+        short daysAtTheFirstOption = (short) (totalWeightGr/weightGr1);
+        System.out.println("Потребуется " +daysAtTheFirstOption+ " при скидывании 250гр веса в день");
+        short daysAtTheSecondOption = (short) (totalWeightGr/weightGr2);
+        System.out.println("Потребуется " +daysAtTheSecondOption+ " при скидывании 500гр веса в день");
+        double averege = totalWeightGr/(weightGr1+weightGr2);
+        System.out.println("Потребуется " +averege+ " дней в среднем");
+
 
 
     }
