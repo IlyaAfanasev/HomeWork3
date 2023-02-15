2eb6da3328d5fd3abad09e47f6a7b92a4166673f
IlyaAfanasev <gecktor.86@mail.ru>
2023-02-15 20:13:15 +0400

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+
+    }
+
+    public static void task1() {
+        byte variableOne = 3;
+        System.out.println("Значение переменной variableOne с типом byte равно " + variableOne);
+        short variableTwo = -13857;
+        System.out.println("Значение переменной variableTwo с типом short равно " + variableTwo);
+        int variableThree = 1359846582;
+        System.out.println("Значение переменной variableThree с типом int равно " + variableThree);
+        long variableFour = -78568478231L;
+        System.out.println("Значение переменной variableFour с типом long равно " + variableFour);
+        float variableFive = 3.14f;
+        System.out.println("Значение переменной variableFive с типом float равно " + variableFive);
+        double variableSix = 587.879642354;
+        System.out.println("Значение переменной variableSix с типом double равно " + variableSix);
+
+
+    }
+}
